d51ef775a9f92d3a6fc2255a1a126683b91908ab
James <140310844+ChanChun9301@users.noreply.github.com>
2025-12-26 10:30:51 +0500

    Update reset_inactive_users.py

--- api/reset_inactive_users.py
+++ api/reset_inactive_users.py
@@ -1,15 +1,22 @@
-# app/management/commands/reset_inactive_users.py
-from django.core.management.base import BaseCommand
-from django.utils import timezone
+# app/reset_inactive_users.py
+import os
+import django
 from datetime import timedelta
+from django.utils import timezone
+
+# Настройка Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings")
+django.setup()
+
 from app.models import UserProd
 
-class Command(BaseCommand):
-    help = "Reset checked=False for inactive users"
+def reset_inactive_users():
+    one_month_ago = timezone.now() - timedelta(days=30)
+    users_updated = UserProd.objects.filter(checked=True, sms_sent_at__lt=one_month_ago).update(checked=False)
+    print(f"Updated {users_updated} users")
 
-    def handle(self, *args, **kwargs):
-        one_month_ago = timezone.now() - timedelta(days=30)
-        users_updated = UserProd.objects.filter(checked=True, sms_sent_at__lt=one_month_ago).update(checked=False)
-        self.stdout.write(f"Updated {users_updated} users")
+if __name__ == "__main__":
+    reset_inactive_users()
+
 
 #  0 0 * * * /path/to/venv/bin/python /path/to/manage.py reset_inactive_users
